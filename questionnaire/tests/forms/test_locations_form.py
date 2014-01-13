bb439202c5e6307ffdcc464d6288962cdd666f1c
Andrew Mukiza <andrewmukiza@gmail.com>
2014-01-13 13:43:48 +0300

    unique locationtype order

--- questionnaire/tests/forms/test_locations_form.py
+++ questionnaire/tests/forms/test_locations_form.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 from questionnaire.forms.locations import LocationTypeForm
+from questionnaire.models import LocationType
 
 
 class LocationTypeFormTest(TestCase):
@@ -20,4 +21,13 @@ class LocationTypeFormTest(TestCase):
         location_type = LocationTypeForm(form_data)
         self.assertFalse(location_type.is_valid())
         message = 'This field is required.'
+        self.assertEqual([message], location_type.errors['order'])
+
+    def test_duplicate_order_is_invalid(self):
+        LocationType.objects.create(name="Region", order=1)
+        form_data = self.form_data.copy()
+        form_data['order'] = 1
+        location_type = LocationTypeForm(form_data)
+        self.assertFalse(location_type.is_valid())
+        message = 'Location type with this Order already exists.'
         self.assertEqual([message], location_type.errors['order'])
--- questionnaire/tests/models/test_locations_model.py
+++ questionnaire/tests/models/test_locations_model.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 from questionnaire.models.locations import LocationType
+from django.db import IntegrityError
 
 
 class LocationTypeTest(TestCase):
@@ -16,3 +17,8 @@ class LocationTypeTest(TestCase):
         self.failUnless(region.id)
         self.assertEqual(1, region.order)
         self.assertIsNone(region.description)
+
+    def test_unique_order(self):
+        region = LocationType.objects.create(name="Region", order=1)
+        another_region = LocationType(name="haha", order=1)
+        self.assertRaises(IntegrityError, another_region.save)
