b2c6411a257e11f66395d424fbc85726e8d4beb4
Andrew Mukiza <andrewmukiza@gmail.com>
2014-01-13 17:17:21 +0300

    changed to travis

---
+++ eJRF/travis-settings.py
@@ -0,0 +1,9 @@
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": "ejrf",
+        "USER": "postgres",
+        "PASSWORD": "",
+        "HOST": "localhost",
+    }
+}
