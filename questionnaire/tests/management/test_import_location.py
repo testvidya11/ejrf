500de906571455ce2c7a46c62d3f65c671aac720
Andrew Mukiza <andrewmukiza@gmail.com>
2014-01-14 17:05:46 +0300

    added custom command to import regions/countries

---
+++ questionnaire/tests/management/test_import_location.py
@@ -0,0 +1,36 @@
+import os
+from questionnaire.models import Region, Country
+from questionnaire.management.commands.import_location import Command
+from questionnaire.tests.base_test import BaseTest
+
+class FakeStdout(object):
+    def write(self, msg):
+        return "haha %s"%msg
+
+class FakeCommand(Command):
+    def __init__(self):
+        super(FakeCommand, self).__init__()
+        self.stdout = FakeStdout()
+
+class ImportLocationTest(BaseTest):
+    def setUp(self):
+        self.data = [['Region', 'Country'],
+                     ['AFRO', 'Uganda'],
+                     ['AFRO', 'Kenya'],
+                     ['PAHO', 'Brazil'],
+                     ['PAHO', 'Mexico']]
+
+        self.write_to_csv('wb', self.data)
+        self.filename = 'test.csv'
+        file = open(self.filename, 'rb')
+        self.importer = FakeCommand()
+
+    def tearDown(self):
+        os.system("rm -rf %s"%self.filename)
+
+    def test_should_create_regions_and_countries(self):
+        self.importer.handle(self.filename)
+        for locations in self.data[1:]:
+            region = Region.objects.filter(name=locations[0])
+            self.failUnless(region)
+            self.failUnless(Country.objects.filter(name=locations[1], region=region[0]))
