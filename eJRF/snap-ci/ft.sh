840276d508f32934bf3240dd20c399df5baa620d
Andrew Mukiza <andrewmukiza@gmail.com>
2014-01-14 15:13:13 +0300

    adding coveralls package to setup and executing coveralls after ft

--- eJRF/snap-ci/ft.sh
+++ eJRF/snap-ci/ft.sh
@@ -2,3 +2,4 @@
 source ../ejrf_env/bin/activate
 cp eJRF/snap-ci/snap-settings.py eJRF/localsettings.py
 ./manage.py harvest
+coveralls
--- eJRF/snap-ci/setup.sh
+++ eJRF/snap-ci/setup.sh
@@ -4,6 +4,7 @@ virtualenv ejrf_env
 source ejrf_env/bin/activate
 cd -
 pip install -r pip-requirements.txt
+pip install coveralls
 cp eJRF/snap-ci/snap-settings.py eJRF/localsettings.py
 ./manage.py syncdb --noinput
 ./manage.py migrate
