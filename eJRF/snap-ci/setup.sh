7092970eaacc333788953215836a3a743b09e0db
Andrew Mukiza <andrewmukiza@gmail.com>
2014-01-14 12:36:43 +0300

    moved all configs and db setup in setup.sh

--- eJRF/snap-ci/setup.sh
+++ eJRF/snap-ci/setup.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
-#virtualenv ejrf_env
-source ../ejrf_env/bin/activate
+cd ..
+virtualenv ejrf_env
+source ejrf_env/bin/activate
+cd -
 pip install -r pip-requirements.txt
 cp eJRF/snap-ci/snap-settings.py eJRF/localsettings.py
+./manage.py syncdb --noinput
+./manage.py migrate
