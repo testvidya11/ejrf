fac8a0fa81196c2b684005a4504c5f00b411f78a
NavaL <mnandri@thoughtworks.com>
2014-01-15 15:58:01 +0300

    added template layout

--- eJRF/settings.py
+++ eJRF/settings.py
@@ -24,6 +24,20 @@ DEBUG = True
 
 TEMPLATE_DEBUG = True
 
+TEMPLATE_LOADERS = (
+    'django.template.loaders.filesystem.Loader',
+    'django.template.loaders.app_directories.Loader',
+#     'django.template.loaders.eggs.Loader',
+)
+
+# List of finder classes that know how to find static files in
+# various locations.
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+#    'django.contrib.staticfiles.finders.DefaultStorageFinder',
+)
+
 ALLOWED_HOSTS = []
 
 
