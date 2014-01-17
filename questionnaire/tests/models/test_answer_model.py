786e7d11aba8c1292ad19130826f24662ed9e204
Remo Samuel Paul <remosam42@gmail.com>
2014-01-17 12:05:44 +0300

    added test validating decimalfield for numericalanswer response

--- questionnaire/tests/models/test_answer_model.py
+++ questionnaire/tests/models/test_answer_model.py
@@ -1,5 +1,6 @@
+from django.core.exceptions import ValidationError
 from django.test import TestCase
-from questionnaire.models import Question, Region, Country
+from questionnaire.models import Question, Country
 from questionnaire.models.answers import Answer, NumericalAnswer
 
 
@@ -15,14 +16,14 @@ class AnswerTest(TestCase):
 
 class NumericalAnswerTest(TestCase):
 
-    def test_answer_fields(self):
+    def test_numerical_answer_fields(self):
         answer = NumericalAnswer()
         fields = [str(item.attname) for item in answer._meta.fields]
         self.assertEqual(6, len(fields))
         for field in ['id', 'created', 'modified', 'question_id','country_id', 'response']:
             self.assertIn(field, fields)
 
-    def test_answer_store(self):
+    def test_numerical_answer_store(self):
         question = Question.objects.create(text='Uganda Revision 2014 what what?', UID='abc123', answer_type='Text')
         country = Country.objects.create(name="Peru")
         answer = NumericalAnswer.objects.create(question=question, country=country, response=11.2)
@@ -30,3 +31,9 @@ class NumericalAnswerTest(TestCase):
         self.assertEqual(question, answer.question)
         self.assertEqual(country, answer.country)
         self.assertEqual(11.2, answer.response)
+
+    def test_numericalanswer_cannot_be_text(self):
+        question = Question.objects.create(text='Uganda Revision 2014 what what?', UID='abc123', answer_type='Text')
+        country = Country.objects.create(name="Peru")
+        answer = NumericalAnswer(question=question, country=country, response='not a decimal number')
+        self.assertRaises(ValidationError, answer.save)
