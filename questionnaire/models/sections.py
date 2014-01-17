46c96ec017ad041aaa48201de2829d3cbfee1253
Andrew Mukiza <andrewmukiza@gmail.com>
2014-01-17 11:26:13 +0300

    [Andrew/Neil] added the sub section model

--- questionnaire/models/sections.py
+++ questionnaire/models/sections.py
@@ -7,3 +7,10 @@ class Section(BaseModel):
     title = models.CharField(max_length=256, blank=False, null=False)
     order = models.IntegerField(blank=False, null=False)
     questionnaire = models.ForeignKey(Questionnaire, blank=False, null=False, related_name="sections")
+
+
+class SubSection(BaseModel):
+    title = models.CharField(max_length=256, blank=False, null=False)
+    order = models.IntegerField(blank=False, null=False)
+    section = models.ForeignKey(Section, blank=False, null=False, related_name="sub_sections")
+    description = models.TextField(blank=True, null=True)
--- questionnaire/tests/models/test_sections_model.py
+++ questionnaire/tests/models/test_sections_model.py
@@ -1,9 +1,16 @@
 from django.test import TestCase
-from questionnaire.models.sections import Section
+from questionnaire.models.sections import Section, SubSection
 from questionnaire.models import Questionnaire
+from questionnaire.tests.base_test import BaseTest
 
 
-class SectionTest(TestCase):
+class SectionBaseTest(BaseTest):
+    def setUp(self):
+        self.questionnaire = Questionnaire.objects.create(name="Uganda Revision 2014", description="some description")
+        self.section = Section.objects.create(title="Immunisation Coverage", order=1, questionnaire=self.questionnaire)
+
+
+class SectionTest(SectionBaseTest):
 
     def test_section_fields(self):
         section = Section()
@@ -12,9 +19,23 @@ class SectionTest(TestCase):
         for field in ['id', 'created', 'modified', 'title', 'order', 'questionnaire_id']:
             self.assertIn(field, fields)
 
-    def test_questionnaire_store(self):
-        questionnaire = Questionnaire.objects.create(name="Uganda Revision 2014", description="some description")
-        section = Section.objects.create(title="Immunisation Coverage", order=1, questionnaire=questionnaire)
-        self.failUnless(section.id)
-        self.assertEqual("Immunisation Coverage", section.title)
-        self.assertEqual(questionnaire, section.questionnaire)
+    def test_section_store(self):
+        self.failUnless(self.section.id)
+        self.assertEqual("Immunisation Coverage", self.section.title)
+        self.assertEqual(self.questionnaire, self.section.questionnaire)
+
+
+class SubSectionTest(SectionBaseTest):
+
+    def test_sub_section_fields(self):
+        sub_section = SubSection()
+        fields = [str(item.attname) for item in sub_section._meta.fields]
+        self.assertEqual(7, len(fields))
+        for field in ['id', 'created', 'modified', 'title', 'order', 'section_id', 'description']:
+            self.assertIn(field, fields)
+
+    def test_sub_section_store(self):
+        sub_section = SubSection.objects.create(title="Infant Immunisation Coverage", order=1, section=self.section)
+        self.failUnless(self.section.id)
+        self.assertEqual("Infant Immunisation Coverage", sub_section.title)
+        self.assertEqual(self.section, sub_section.section)
