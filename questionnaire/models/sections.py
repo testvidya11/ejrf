fc30e0387d850806ade63d9236aa37ffd2ff9ac3
Neil Coutinho <ncoutin@thoughtworks.com>
2014-01-20 16:27:30 +0300

    Added description field to section.

--- questionnaire/models/sections.py
+++ questionnaire/models/sections.py
@@ -7,6 +7,7 @@ class Section(BaseModel):
     name = models.CharField(max_length=100, blank=False, null=True)
     title = models.CharField(max_length=256, blank=False, null=False)
     order = models.IntegerField(blank=False, null=False)
+    description = models.TextField(blank=True, null=True)
     questionnaire = models.ForeignKey(Questionnaire, blank=False, null=False, related_name="sections")
 
 
