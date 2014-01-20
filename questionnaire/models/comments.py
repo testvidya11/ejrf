1d3a01ffa0efec42277c479323c1b9df30ccfaf8
NavaL <mnandri@thoughtworks.com>
2014-01-20 19:09:22 +0300

    now comments are M2M to answer_groups instead of answer

--- questionnaire/models/comments.py
+++ questionnaire/models/comments.py
@@ -5,5 +5,5 @@ from django.db import models
 
 class Comment(BaseModel):
     text = models.CharField(max_length=100, blank=False, null=False)
-    answer = models.ManyToManyField("Answer", blank=False, null=False, related_name="comments")
+    answer_group = models.ManyToManyField("AnswerGroup", blank=False, null=False, related_name="comments")
     user = models.ForeignKey(User, blank=False, null=False)
--- questionnaire/tests/models/test_comments_model.py
+++ questionnaire/tests/models/test_comments_model.py
@@ -1,5 +1,5 @@
 from django.contrib.auth.models import User
-from questionnaire.models import Question, Country, NumericalAnswer
+from questionnaire.models import Question, Country, NumericalAnswer, AnswerGroup, QuestionGroup, SubSection, Section, Questionnaire
 from questionnaire.models.comments import Comment
 from questionnaire.tests.base_test import BaseTest
 
@@ -7,11 +7,17 @@ from questionnaire.tests.base_test import BaseTest
 class CommentTest(BaseTest):
 
     def setUp(self):
+        self.questionnaire = Questionnaire.objects.create(name="Uganda Revision 2014", description="some description")
         self.question = Question.objects.create(text='Uganda Revision 2014 what what?', UID='abc123', answer_type='Text')
         self.country = Country.objects.create(name="Peru")
         self.tony = User.objects.create(username="Tony")
         self.montana = User.objects.create(username="Montana")
+        self.section = Section.objects.create(title="Immunisation Coverage", order=1, questionnaire=self.questionnaire)
+        self.sub_section = SubSection.objects.create(title="Immunisation Extra Coverage", order=1, section=self.section)
+        self.grouped_question = QuestionGroup.objects.create(subsection=self.sub_section, order=1)
         self.answer = NumericalAnswer.objects.create(question=self.question, country=self.country, response=11.2)
+        self.answer_group = AnswerGroup.objects.create(answer=self.answer,
+                                                       grouped_question=self.grouped_question, row=1)
 
     def test_comment_fields(self):
         comment = Comment()
@@ -23,9 +29,9 @@ class CommentTest(BaseTest):
     def test_comments_store(self):
         tony_comment = Comment.objects.create(user=self.tony, text="This is an absurd figure")
         montana_comment = Comment.objects.create(user=self.montana, text="This is an absurd figure")
-        self.answer.comments.add(tony_comment)
-        self.answer.comments.add(montana_comment)
-        answer_comments = self.answer.comments.all()
+        self.answer_group.comments.add(tony_comment)
+        self.answer_group.comments.add(montana_comment)
+        answer_comments = self.answer_group.comments.all()
 
         self.failUnless(tony_comment.id)
         self.failUnless(montana_comment.id)
