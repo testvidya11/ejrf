d0c45b445bd5e12706cf9971e3b8ec3a173c51a1
Andrew Mukiza <andrewmukiza@gmail.com>
2014-02-03 08:29:23 +0300

    adding the bootstrap styles on filter form

--- questionnaire/forms/filter.py
+++ questionnaire/forms/filter.py
@@ -3,4 +3,5 @@ from questionnaire.models import Region
 
 
 class UserFilterForm(forms.Form):
-    region = forms.ModelChoiceField(queryset=Region.objects.all().order_by('name'), empty_label=None)
+    region = forms.ModelChoiceField(queryset=Region.objects.all().order_by('name'), empty_label="Select a region",
+                                    widget=forms.Select(attrs={"class": 'form-control'}))
