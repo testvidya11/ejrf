cf34565e8b777f7abfe9a57b59d17090215d5f07
Andrew Mukiza <andrewmukiza@gmail.com>
2014-02-19 18:20:06 +0300

    Refactored the home page view

--- questionnaire/views/home.py
+++ questionnaire/views/home.py
@@ -8,22 +8,26 @@ from questionnaire.models import Questionnaire, Section
 
 
 class Home(MultiplePermissionsRequiredMixin, View):
-    permissions = {'any': ('auth.can_submit_responses', 'auth.can_view_users')}
-    data_submitter_template = "home/index.html"
-    template_name = data_submitter_template
+    def __init__(self, *args, **kwargs):
+        super(Home, self).__init__(*args, **kwargs)
+        self.permissions = {'any': ('auth.can_submit_responses', 'auth.can_view_users')}
+        self.template_name = "home/index.html"
+        self.questionnaires = Questionnaire.objects.all()
+        self.sections = Section.objects.order_by('order')
 
-    def get(self, request, *args, **kwargs):
+    def get(self, *args, **kwargs):
+        if self.request.user.has_perm('auth.can_view_users'):
+            return self._render_global_admin_home()
+        if self.questionnaires.exists() and self.sections.exists():
+            return self._render_questionnaire_section()
+        return render(self.request, self.template_name)
 
-        if request.user.has_perm('auth.can_view_users'):
-            global_admin_template = 'home/global/index.html'
-            questionnaires = Questionnaire.objects.all()
-            context = {'finalized_questionnaires': questionnaires.filter(finalized=True),
-                       'not_finalized_questionnaires': questionnaires.filter(finalized=False)}
-            return render(request, global_admin_template, context)
+    def _render_global_admin_home(self):
+        global_admin_template = 'home/global/index.html'
+        context = {'finalized_questionnaires': self.questionnaires.filter(finalized=True),
+                   'not_finalized_questionnaires': self.questionnaires.filter(finalized=False)}
+        return render(self.request, global_admin_template, context)
 
-        questionnaires = Questionnaire.objects.all()
-        sections = Section.objects.order_by('order')
-        if questionnaires.exists() and sections.exists():
-            args = (questionnaires.latest('created').id, sections[0].id)
-            return HttpResponseRedirect(reverse('questionnaire_entry_page', args=args))
-        return render(request, self.template_name)
+    def _render_questionnaire_section(self):
+        args = (self.questionnaires.latest('created').id, self.sections[0].id)
+        return HttpResponseRedirect(reverse('questionnaire_entry_page', args=args))
