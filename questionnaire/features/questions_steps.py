a7c3618f286f30083a0f4456954772583cd2fb42
Andrew Mukiza <andrewmukiza@gmail.com>
2014-02-19 15:32:13 +0300

    [Andrew/Neil] added export label on create question

--- questionnaire/features/questions_steps.py
+++ questionnaire/features/questions_steps.py
@@ -6,7 +6,7 @@ from questionnaire.models import Question
 @step(u'And I have 100 questions')
 def and_i_have_100_questions(step):
     for i in range(0, 100):
-        Question.objects.create(text="When will you be %s years old" % i,
+        Question.objects.create(text="When will you be %s years old" % i, export_label="Export text for %s" % i,
                                 instructions="question %s answer sensibly" % i, UID="%s" % i)
 
 @step(u'And I visit the question listing page')
@@ -17,7 +17,7 @@ def and_i_visit_the_question_listing_page(step):
 @step(u'Then I should see all questions paginated')
 def then_i_should_see_all_questions_paginated(step):
     for i in range(0, 4):
-        world.page.is_text_present("When will you be %s years old" % i, instructions="question %s answer sensibly" % i, UID="%s" % i)
+        world.page.is_text_present("Export text for %s" % i,  "%s" % i)
     world.page.click_link_by_text("2")
 
 @step(u'And I click add new question page')
