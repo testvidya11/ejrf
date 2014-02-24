acc5fac58f065c2be96a4d347bf06e135a6ff30e
Neil Coutinho <ncoutin@thoughtworks.com>
2014-02-24 15:28:15 +0300

    [Neil/Andrew] refactored the questionnaire cloner service service

--- questionnaire/services/questionnaire_cloner.py
+++ questionnaire/services/questionnaire_cloner.py
@@ -7,7 +7,6 @@ class QuestionnaireClonerService(object):
         self.questionnaire = questionnaire
         self.original_questionnaire = copy.deepcopy(self.questionnaire)
         self.sections = None
-        self.sections_mapping = None
         self.sub_sections = None
         self.question_groups = None
 
@@ -20,32 +19,27 @@ class QuestionnaireClonerService(object):
         return self.questionnaire, self.original_questionnaire
 
     def clone_sections(self):
-        sections = self.original_questionnaire.sections.all().values('name', 'title', 'description', 'order')
-        bulk_sections = [Section.objects.create(questionnaire=self.questionnaire, **section_data) for section_data in sections]
+        sections = self.original_questionnaire.sections.all()
+        bulk_sections = dict((section, Section.objects.create(questionnaire=self.questionnaire, **self._querable_fields(section, ['name', 'title', 'description', 'order']))) for section in sections)
         return bulk_sections
 
     def clone_sub_sections(self):
-        sub_sections = []
-        fields_to_query = ['title', 'description', 'order']
-        for section in self.sections:
-            query_params = self._get_fields_to_query(fields_to_query, section)
-            old_section = Section.objects.filter(questionnaire=self.original_questionnaire, **query_params)
-            for data in old_section[0].sub_sections.all().values('title', 'description', 'order'):
-                sub_sections.extend([SubSection.objects.create(section=section, **data)])
-        return sub_sections
+        sub_sections_map = {}
+        for old_section, new_section in self.sections.items():
+            sub_sections = old_section.sub_sections.all()
+            sub_sections_map.update(dict((sub_section, SubSection.objects.create(section=new_section, **self._querable_fields(sub_section, ['title', 'description', 'order']))) for sub_section in sub_sections))
+        return sub_sections_map
 
     def clone_question_groups(self):
-        question_groups = []
-        fields_to_query = ['name', 'instructions', 'parent', 'order', 'allow_multiples']
-        sections_all = self.original_questionnaire.sections.all()
-        for section in sections_all:
-            for sub_section in section.sub_sections.all():
-                query_params = self._get_fields_to_query(fields_to_query, sub_section)
-                old_sub_sections = SubSection.objects.filter(section=section, **query_params)
-                for data in old_sub_sections[0].all_question_groups().values('name', 'instructions', 'parent', 'order', 'allow_multiples'):
-                    question_groups.extend([QuestionGroup.objects.create(subsection=sub_section, **data)])
-        return question_groups
+        question_groups_map = {}
+        for old_sub_section, new_sub_section in self.sub_sections.items():
+            question_groups = old_sub_section.all_question_groups()
+            question_groups_map.update(dict((question_group, QuestionGroup.objects.create(subsection=new_sub_section, **self._querable_fields(question_group, ['name', 'instructions', 'parent', 'order', 'allow_multiples']))) for question_group in question_groups))
+        return question_groups_map
 
 
     def _get_fields_to_query(self, fields_to_query, model):
         return dict((key, value) for key, value in model.__dict__.iteritems() if value and key in fields_to_query)
+
+    def _querable_fields(self, model, fields):
+        return dict((field, getattr(model,field)) for field in fields)
